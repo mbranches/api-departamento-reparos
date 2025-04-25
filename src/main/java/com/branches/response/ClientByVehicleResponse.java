5bef8e6db55e10592026d3edff4272cd8e67f0aa
mbranches <marcusvbranches@gmail.com>
2025-04-25 13:30:11 -0300

    refactor: renomeia classe

--- src/main/java/com/branches/response/ClientByVehicleResponse.java
+++ src/main/java/com/branches/response/ClientByVehicleResponse.java
@@ -5,7 +5,8 @@ import lombok.Data;
 
 @Data
 @Builder
-public class ClientByVehicleGetResponse {
+public class ClientByVehicleResponse {
+    private Long id;
     private String name;
     private String lastName;
 }
