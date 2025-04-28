badac0d2b2e24cfb5c6afbfdffd2dfe9082fc4a6
mbranches <marcusvbranches@gmail.com>
2025-04-28 12:41:55 -0300

    feat: adiciona validação para garantir numeros positivos

--- src/main/java/com/branches/request/RepairEmployeeByRepairPostRequest.java
+++ src/main/java/com/branches/request/RepairEmployeeByRepairPostRequest.java
@@ -11,5 +11,6 @@ public class RepairEmployeeByRepairPostRequest {
     @NotNull(message = "The field 'employeeId' cannot be null")
     private Long employeeId ;
     @NotNull(message = "The field 'hoursWorked' cannot be null")
+    @PositiveOrZero(message = "'hoursWorked' must be equal to or greater than 0")
     private Integer hoursWorked;
 }
