e83cbf6c7bbd19ad29524f6a9fd6d8e201cb160f
mbranches <marcusvbranches@gmail.com>
2025-05-03 09:08:05 -0300

    refactor: atualiza mensagem de error

--- src/main/java/com/branches/request/ClientPostRequest.java
+++ src/main/java/com/branches/request/ClientPostRequest.java
@@ -11,9 +11,9 @@ import java.util.List;
 @Data
 @Builder
 public class ClientPostRequest {
-    @NotBlank(message = "The field name is required")
+    @NotBlank(message = "The field 'name' is required")
     private String name;
-    @NotBlank(message = "The field lastName is required")
+    @NotBlank(message = "The field 'lastName' is required")
     private String lastName;
     private String email;
     private Address address;
