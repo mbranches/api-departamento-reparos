da28e4ff6c378ad8689aa619de1c2fb54b41fa41
mbranches <marcusvbranches@gmail.com>
2025-05-03 09:08:47 -0300

    feat: torna email obrigatório a client

--- src/main/java/com/branches/request/ClientPostRequest.java
+++ src/main/java/com/branches/request/ClientPostRequest.java
@@ -2,6 +2,7 @@ package com.branches.request;
 
 import com.branches.model.Address;
 import com.branches.model.Phone;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,8 @@ public class ClientPostRequest {
     private String name;
     @NotBlank(message = "The field 'lastName' is required")
     private String lastName;
+    @NotBlank(message = "The field 'email' is required")
+    @Email(regexp = "^(?!.*\\.\\.)([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\\.([a-zA-Z]{2,})$", message = "Email is not valid")
     private String email;
     private Address address;
     private List<Phone> phones;
